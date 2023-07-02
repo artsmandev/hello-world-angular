31310b94b69af844e8f27586e5aaa5828fe567fe
Bruno Andrade <bruno@artsman.dev>
2023-07-02 15:53:53 +0100

    (feat): both components together

--- hello-world/src/app/warning-alert/warning-alert.component.ts
+++ hello-world/src/app/warning-alert/warning-alert.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
 	selector: 'app-warning-alert',
+	styleUrls: ['./warning-alert.component.css'],
 	templateUrl: './warning-alert.component.html'
 })
 export class WarningAlertComponent {}
