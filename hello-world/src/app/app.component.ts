a32bedc3163bc335e90c8fbf2b56a62e7d67f139
Bruno Andrade <bruno@artsman.dev>
2023-08-26 18:57:06 +0100

    feat: combining all forms data binding

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -6,21 +6,5 @@ import {Component} from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	buttonEnabled = false;
-	clickButtonMessage = 'The button was not clicked';
-	inputText = '[Default input text]'
-
-	constructor() {
-		setTimeout(() => {
-			this.buttonEnabled = true;
-		}, 2000);
-	}
-
-	onClickButton() {
-		this.clickButtonMessage = 'The button was clicked!';
-	}
-
-	onInputUpdate(event: Event) {
-		this.inputText = (<HTMLInputElement>event.target).value;
-	}
+	username = '';
 }
